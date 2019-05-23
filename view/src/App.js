7d925c62ee895ea7566dcb966890970ce34bec96
Sungjun <hongsi140626@gmail.com>
2019-05-22 23:06:07 -0400

    practice for merge

--- view/src/App.js
+++ view/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home/Home';
+import Home from './components/Home';
 import Product from './components/Product/Product';
 
 export default class App extends React.Component{
