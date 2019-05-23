579031b51218da59809189a0fd7f6fd35b2e1d34
Sungjun <hongsi140626@gmail.com>
2019-05-23 00:09:25 -0400

    branch v3

--- view/src/App.js
+++ view/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home/Home';
+import Home from './components/Home';
 import Product from './components/Product/Product';
-abcdefg
+abcdefghong
 export default class App extends React.Component{
   render(){
     return(
